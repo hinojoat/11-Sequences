df594d3df1326b3a2892d8994eeb0f82b267a657
hinojoat <hinojoat@rose-hulman.edu>
2018-12-19 14:42:34 -0500

    WIP

--- src/m3_iterating.py
+++ src/m3_iterating.py
@@ -5,8 +5,8 @@ in its most classic form:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Alex Hinojosa.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -21,7 +21,7 @@ def main():
 def run_test_count_negatives():
     """ Tests the   count_negatives   function. """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  count_negatives  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests beyond those we wrote.
     #
@@ -83,9 +83,20 @@ def run_test_count_negatives():
     print('       actual:  ', actual)
 
     # -------------------------------------------------------------------------
-    # TODO 2 (continued):  Add your 2 ADDITIONAL tests here:
+    # DONE 2 (continued):  Add your 2 ADDITIONAL tests here:
     # -------------------------------------------------------------------------
 
+    expected = 2
+    actual = count_negatives([4, 7, -3, -2])
+    print()
+    print('Test 6 expected:', expected)
+    print('       actual:', actual)
+
+    expected = 4
+    actual = count_negatives([-3, -2, -5, -10])
+    print()
+    print('Test 7 expected:', expected)
+    print('       actual:', actual)
 
 def count_negatives(seq):
     """
@@ -103,15 +114,19 @@ def count_negatives(seq):
       :type seq: (list | tuple) of (int | float)
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # -------------------------------------------------------------------------
-
+    total = 0
+    for k in range(len(seq)):
+        if (seq[k]) < 0:
+            total = total + 1
+    return total
 
 def run_test_count_short_ones():
     """ Tests the   count_short_ones   function. """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement this TEST function.
+    # DONE: 4. Implement this TEST function.
     #   It TESTS the  count_short_ones  function defined below.
     #   Include at least ** 2 ** ADDITIONAL tests beyond those we wrote.
     #
@@ -187,7 +202,19 @@ def run_test_count_short_ones():
     # -------------------------------------------------------------------------
     # TODO 4 (continued):  Add your 2 ADDITIONAL test(s) here:
     # -------------------------------------------------------------------------
+    expected = 3
+    seq = [[], [4, 5, 6], [38, 290], [2, 4, 6, 8, 10]]
+    actual = count_short_ones(seq)
+    print()
+    print('Test 8 expected:', expected)
+    print('       actual', actual)
 
+    expected = 2
+    seq = [[2, 3, 5, 7, 8], [4, 5, 6], [38, 290], [2, 4, 6, 8, 10]]
+    actual = count_short_ones(seq)
+    print()
+    print('Test 8 expected:', expected)
+    print('       actual', actual)
 
 def count_short_ones(seq_of_lists):
     """
@@ -213,6 +240,11 @@ def count_short_ones(seq_of_lists):
     # TODO: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
+    total = 0
+    for k in range(len(seq_of_lists)):
+        if k < 3:
+            total = total + 1
+
 
 
 def run_test_draw_circles():
